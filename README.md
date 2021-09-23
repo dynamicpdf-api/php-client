845f093b8068ce52d6c39e472dc91f303e2cb1fd
Ashutosh Raghuwanshi <raghuwanshi@users.noreply.github.com>
2021-09-23 20:08:09 +0530

    Updated for initial release.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# php-client
-PHP client library for the DynamicPDF Cloud API.
+DynamicPDF Cloud API - PHP Client
+=================================
+
+PHP client library for the DynamicPDF Cloud API. For more information please visit [DynamicPDF Cloud API](https://cloud.dynamicpdf.com/ "DynamicPDF Cloud API Homepage")
+
+Support for other languages/plateforms can be found at [GitHub](https://github.com/dynamicpdf-api "DynamicPDF Cloud API at GitHub") 
--- tests/EndPoints/TestParameters.php
+++ tests/EndPoints/TestParameters.php
@@ -1,15 +1,13 @@
 <?php
 namespace DynamicPDF\Api;
 
-namespace DynamicPDF\Api;
-
 class TestParameters
 {
 
-    public const Inputpath = __DIR__ . "./../Resources/";
-    public const OutPutPath = __DIR__ . "./Output/";
-    public const Key = "DP.04XCRJfZOpktQAEOlT7o4LmzhsvGDcQcpnpSKI6bwB/ZRZtuMDV42WyS";
-    public const Url = "https://localhost:44397/v1.0";
-    public const Author = "test";
-    public const Title = "test";
-}
+  public const Inputpath =  __DIR__ . "./../Resources/";
+  public const OutPutPath =  __DIR__ . "./Output/";
+  public const Key = "[YOUR API KEY GOES HERE]";
+  public const Url = "https://api.dynamicpdf.com/v1.0";
+  public const Author = "test";
+  public const Title = "test";
+};
